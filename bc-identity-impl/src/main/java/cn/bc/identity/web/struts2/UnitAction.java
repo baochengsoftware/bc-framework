1a4ef56b551b2ca4d1475d2f2ddb61b97952253f
rongjihuang <rongjihuang@gmail.com>
2011-05-03 17:51:04 +0800

    单位列表中的条件过滤

--- bc-identity-impl/src/main/java/cn/bc/identity/web/struts2/UnitAction.java
+++ bc-identity-impl/src/main/java/cn/bc/identity/web/struts2/UnitAction.java
@@ -9,6 +9,11 @@ import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
+import cn.bc.core.query.condition.Condition;
+import cn.bc.core.query.condition.Direction;
+import cn.bc.core.query.condition.impl.AndCondition;
+import cn.bc.core.query.condition.impl.EqualsCondition;
+import cn.bc.core.query.condition.impl.OrderCondition;
 import cn.bc.identity.domain.Actor;
 import cn.bc.web.ui.html.grid.Grid;
 import cn.bc.web.ui.html.grid.TextColumn;
@@ -31,8 +36,10 @@ public class UnitAction extends AbstractActorAction {
 	}
 
 	protected Grid buildGrid(List<Actor> entities) {
-		return super.buildGrid(entities)
-				.addColumn(new TextColumn("status", getText("actor.status"), 40))
+		return super
+				.buildGrid(entities)
+				.addColumn(
+						new TextColumn("status", getText("actor.status"), 40))
 				.addColumn(new TextColumn("code", getText("actor.code"), 80))
 				.addColumn(new TextColumn("name", getText("actor.name")))
 				.addColumn(new TextColumn("phone", getText("actor.phone"), 120))
@@ -40,6 +47,17 @@ public class UnitAction extends AbstractActorAction {
 	}
 
 	protected Option buildListPageOption() {
-		return super.buildListPageOption().setWidth(650).setMinWidth(500).setMinHeight(300);
+		return super.buildListPageOption().setWidth(650).setMinWidth(500)
+				.setMinHeight(300);
+	}
+
+	public String list() throws Exception {
+		Condition condition = new AndCondition().add(
+				new EqualsCondition("type", new Integer(Actor.TYPE_UNIT))).add(
+				new OrderCondition("code", Direction.Asc));
+		this.setEntities(this.getCrudService().createQuery()
+				.condition(condition).list());
+		this.setPage(buildListPage(this.getEntities()));
+		return "list";
 	}
 }
